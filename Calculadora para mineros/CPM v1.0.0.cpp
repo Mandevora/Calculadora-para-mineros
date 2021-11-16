e8fcc6479674cbde150003f711730ca76ff960fe
Mandevora <75262868+Mandevora@users.noreply.github.com>
2021-11-15 18:12:43 -0600

    Add files via upload

---
+++ Calculadora para mineros/CPM v1.0.0.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+
+using namespace std;
+int main(){
+	int opcion;
+	float watios, kwh1, b, c, d;
+	
+while(opcion!=0){
+	system("cls");
+	cout<<"CALCULADORA PARA MINEROS"<<endl;
+	cout<<"************************"<<endl;
+	cout<<"¿que decea hacer?"<<endl;
+	cout<<"Calcular el consumo diario de una grafica [1]"<<endl;
+	cout<<" "<<endl; //espacio
+	cout<<"Salir                                     [0]"<<endl;
+	cout<<" "<<endl; //espacio
+	cout<<"Seleccione una opcion: ";
+		
+	cin>>opcion;
+	
+	switch(opcion){
+		case 1: //Calcular el consumo de una grafica
+			cout<<" "<<endl; //espacio
+			cout<<"Introdusca el consumo de su grafica en watios: ";
+			cin>>watios;
+			cout<<"Porfavor introdusca el valor del kWh de su region: ";
+			cin>>kwh1;
+			
+			
+			b = watios / 1000 * 24 * kwh1;
+			
+			
+			cout<<" "<<endl; //espacio
+			cout<<"Su grafica gastara "<<b<<" pesos cada 24 horas"<<endl;;
+		break;
+		
+		case 0:
+			;
+		break;
+	}
+	system("pause");
+	
+		
+		
+}
+
+
+	
+	
+	return 0;
+}
